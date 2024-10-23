88573dd37f71a66220e07f30e05a56f9a26ea114
hazemone <hazemone@hotmail.com>
2024-10-23 10:38:52 +0100

    New files

--- Simple_scaffold_desgin/lib/main.dart
+++ Simple_scaffold_desgin/lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
           title: Text("University of Sunderland"),
           backgroundColor: Colors.amber,
           leading: Icon(Icons.menu),
+          flexibleSpace: Image.network('https://w7.pngwing.com/pngs/863/158/png-transparent-national-glass-centre-university-of-sunderland-cyprus-international-university-student-ksa-text-people-logo.png')
         ),
         body: Center( 
           child: Container(
@@ -26,14 +27,52 @@ class MyApp extends StatelessWidget {
             borderRadius: BorderRadius.circular(20)
           ),
           padding: EdgeInsets.all(25),
-          child: Text("Hazem Eissa", style: TextStyle(color: Colors.white)),
+          child: Text("CET373 - Mobile Technologies", style: TextStyle(color: Colors.white, height: 8, fontSize: 17), textAlign: TextAlign.center,),
           ),
         ),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.thumb_up),
           onPressed: () => {},
         ),
+        /*drawer: Drawer(
+          child: ListView(),
+        ),*/
       ),
     );
   }
+
+      // This part is desgined to show a table for salesman, I comment it to separate the top desgin from this one.
+
+ /* @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text("University of Sunderland"),
+          backgroundColor: Colors.amber,
+          leading: Icon(Icons.menu),
+          flexibleSpace: Image.network('https://w7.pngwing.com/pngs/863/158/png-transparent-national-glass-centre-university-of-sunderland-cyprus-international-university-student-ksa-text-people-logo.png')
+        ),
+      body: Table(
+      children: <TableRow>[
+        TableRow(children: <Widget>[
+          Text('Salesman', style: TextStyle(fontWeight: FontWeight.bold)),
+          Text('jan', style: TextStyle(fontWeight: FontWeight.bold)),
+          Text('feb', style: TextStyle(fontWeight: FontWeight.bold)),
+        ]),
+        TableRow(children: <Widget>[
+          Text('Ahmed'),
+          Text('2050'),
+          Text('1949'),
+      ]),
+      TableRow(children: <Widget>[
+          Text('Omar'),
+          Text('10544'),
+          Text('5042'),
+      ]),
+      ])
+      )
+    );
+  }*/
 }
